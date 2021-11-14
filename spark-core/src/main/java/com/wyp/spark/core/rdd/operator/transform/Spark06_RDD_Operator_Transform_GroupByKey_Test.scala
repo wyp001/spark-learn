24e0d8dcaef05cf95aa05d18007f1095090d4631
wyupeng <wangyupeng_ly@163.com>
2021-11-14 14:13:51 +0800

    054.SparkCore groupBy 从 apache.log 日志数据中获取每个时间段访问量

---
+++ spark-core/src/main/java/com/wyp/spark/core/rdd/operator/transform/Spark06_RDD_Operator_Transform_GroupByKey_Test.scala
@@ -0,0 +1,41 @@
+package com.wyp.spark.core.rdd.operator.transform
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * 算子 - groupBy
+ * 从 apache.log 日志数据中获取每个时间段访问量
+ */
+object Spark06_RDD_Operator_Transform_GroupByKey_Test {
+
+    def main(args: Array[String]): Unit = {
+
+        val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+        val sc = new SparkContext(sparkConf)
+
+        val rdd = sc.textFile("datas/apache.log")
+
+        val timeRDD: RDD[(String, Iterable[(String, Int)])] = rdd.map(
+            line => {
+                val datas = line.split(" ")
+                val time = datas(3)
+                val sdf = new SimpleDateFormat("dd/MM/yyyy:HH:mm:ss")
+                val date: Date = sdf.parse(time)
+                val sdf1 = new SimpleDateFormat("HH")
+                val hour: String = sdf1.format(date)
+                (hour, 1)
+            }
+        ).groupBy(_._1)
+        timeRDD.map{
+            case ( hour, iter ) => {
+                (hour, iter.size)
+            }
+        }.collect.foreach(println)
+
+        sc.stop()
+    }
+}
