7ebb583472468de27f1e08fda682082863f4bb40
wyupeng <wangyupeng_ly@163.com>
2021-11-14 19:40:36 +0800

    060.SparkCore - 核心编程 - RDD - 转换算子 - sortBy

---
+++ spark-core/src/main/java/com/wyp/spark/core/rdd/operator/transform/Spark12_RDD_Operator_Transform1_sortBy.scala
@@ -0,0 +1,22 @@
+package com.wyp.spark.core.rdd.operator.transform
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark12_RDD_Operator_Transform1_sortBy {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+    val sc = new SparkContext(sparkConf)
+
+    val rdd = sc.makeRDD(List(("1", 1), ("11", 2), ("2", 3)), 2)
+
+    // sortBy方法可以根据指定的规则对数据源中的数据进行排序，默认为升序，第二个参数可以改变排序的方式
+    // sortBy默认情况下，不会改变分区。但是中间存在shuffle操作
+    val newRDD = rdd.sortBy(t => t._1.toInt, false)
+
+    newRDD.collect().foreach(println)
+
+    sc.stop()
+  }
+}
