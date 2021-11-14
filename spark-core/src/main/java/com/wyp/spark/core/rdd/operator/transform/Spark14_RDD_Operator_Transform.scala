c29437b2f7db168523addb6b29812ef9003afe7d
wyupeng <wangyupeng_ly@163.com>
2021-11-14 20:13:08 +0800

    063.SparkCore - 核心编程 - RDD - 转换算子 - partitionBy（前面有吸气，中间有等待）

---
+++ spark-core/src/main/java/com/wyp/spark/core/rdd/operator/transform/Spark14_RDD_Operator_Transform.scala
@@ -0,0 +1,30 @@
+package com.wyp.spark.core.rdd.operator.transform
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
+
+/**
+ * 算子 - (Key - Value类型)
+ */
+object Spark14_RDD_Operator_Transform {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+    val sc = new SparkContext(sparkConf)
+
+    val rdd: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4), 2)
+
+    val mapRDD: RDD[(Int, Int)] = rdd.map((_, 1))
+    // RDD => PairRDDFunctions
+    // 隐式转换（二次编译）
+
+    // partitionBy根据指定的分区规则对数据进行重分区
+    val newRDD: RDD[(Int, Int)] = mapRDD.partitionBy(new HashPartitioner(2))
+    newRDD.partitionBy(new HashPartitioner(2))
+
+    newRDD.saveAsTextFile("output")
+
+    sc.stop()
+  }
+}
