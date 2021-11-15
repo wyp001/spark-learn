36a441bedb2a5c2aca991cb6d3d0c2285cb60537
wyupeng <wangyp@trgroup.cn>
2021-11-15 14:37:12 +0800

    083.SparkCore - 核心编程 - RDD - 行动算子 - fold

--- spark-core/src/main/java/com/wyp/spark/core/rdd/operator/action/Spark03_RDD_Operator_Action_aggregate.scala
+++ spark-core/src/main/java/com/wyp/spark/core/rdd/operator/action/Spark03_RDD_Operator_Action_aggregate.scala
@@ -14,7 +14,10 @@ object Spark03_RDD_Operator_Action_aggregate {
     //10 + 13 + 17 = 40
     // aggregateByKey : 初始值只会参与分区内计算
     // aggregate : 初始值会参与分区内计算,并且和参与分区间计算
-    val result = rdd.aggregate(10)(_ + _, _ + _)
+//    val result = rdd.aggregate(10)(_ + _, _ + _)
+
+    // 当aggregate 指定的分区内和分区间的计算方式一样时，可以使用 fold 算子进行简化
+    val result = rdd.fold(10)(_ + _)
 
     println(result)
 
