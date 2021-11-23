776e6d15f142f56901daf55627dee46fea74972f
wyupeng <wangyp@trgroup.cn>
2021-11-23 17:58:05 +0800

    165.SparkSQL - 核心编程 - IDEA - DataFrame基本操作

--- spark-core/src/main/java/com/wyp/spark/sql/Spark01_SparkSQL_Basic.scala
+++ spark-core/src/main/java/com/wyp/spark/sql/Spark01_SparkSQL_Basic.scala
@@ -13,7 +13,22 @@ object Spark01_SparkSQL_Basic {
     val spark: SparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
 
     // 2、执行逻辑操作
+    // 构建 DataFrame
+    val df: DataFrame = spark.read.json("datas/user.json")
+    //df.show()
 
+    // DataFrame => SQL
+    //df.createOrReplaceTempView("user")
+    //spark.sql("select * from user").show()
+    //spark.sql("select age, username from user").show()
+    //spark.sql("select avg(age) from user").show()
+
+    // DataFrame => DSL
+    // 在使用DataFrame时，如果涉及到转换操作，需要引入转换规则
+    import spark.implicits._    // 引入隐式转换规则，其中的spark 为 SparkSession 的实例名称
+    df.select("age", "username").show
+    df.select($"age" + 1).show
+    df.select('age + 1).show
 
     // /3、关闭环境
     spark.close()
