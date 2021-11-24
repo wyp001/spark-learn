314cb025d6a6612ace47898664f8a030ee9dbfc6
wyupeng <wangyp@trgroup.cn>
2021-11-24 09:08:52 +0800

    167.SparkSQL - 核心编程 - IDEA - RDD & DataFrame & DataSet互相转换

--- spark-core/src/main/java/com/wyp/spark/sql/Spark01_SparkSQL_Basic.scala
+++ spark-core/src/main/java/com/wyp/spark/sql/Spark01_SparkSQL_Basic.scala
@@ -15,7 +15,7 @@ object Spark01_SparkSQL_Basic {
 
     // 2、执行逻辑操作
     // 构建 DataFrame
-    val df: DataFrame = spark.read.json("datas/user.json")
+    //val df: DataFrame = spark.read.json("datas/user.json")
     //df.show()
 
     // DataFrame => SQL
@@ -33,11 +33,32 @@ object Spark01_SparkSQL_Basic {
     // DataSet
     // 构建DataSet
     // DataFrame其实是特定泛型的DataSet
-    val seq = Seq(1, 2, 3, 4)
-    val ds: Dataset[Int] = seq.toDS()
-    ds.show()
+    //val seq = Seq(1, 2, 3, 4)
+    //val ds: Dataset[Int] = seq.toDS()
+    //ds.show()
+
+    // RDD <=> DataFrame
+    val rdd: RDD[(Int, String, Int)] = spark.sparkContext.makeRDD(List((1, "zhangsan", 30), (2, "lisi", 40)))
+    val df: DataFrame = rdd.toDF("id", "name", "age")
+    val rowRDD: RDD[Row] = df.rdd
+
+    // DataFrame <=> DataSet
+    val ds: Dataset[User] = df.as[User]
+    val df1: DataFrame = ds.toDF()
+
+    // RDD <=> DataSet
+    val ds1: Dataset[User] = rdd.map {
+      case (id, name, age) => {
+        User(id, name, age)
+      }
+    }.toDS()
+    val userRDD: RDD[User] = ds1.rdd
 
     // /3、关闭环境
     spark.close()
   }
+
+  case class User( id:Int, name:String, age:Int )
+
+
 }
