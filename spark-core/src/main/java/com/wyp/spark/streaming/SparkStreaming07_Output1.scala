0cfb79ba862dc28bb391eda4a0c1879af5ddbe6a
wyupeng <wangyp@trgroup.cn>
2021-11-25 11:08:51 +0800

    198.SparkStreaming - DStream输出

---
+++ spark-core/src/main/java/com/wyp/spark/streaming/SparkStreaming07_Output1.scala
@@ -0,0 +1,39 @@
+package com.wyp.spark.streaming
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object SparkStreaming07_Output1 {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkStreaming")
+    val ssc = new StreamingContext(sparkConf, Seconds(3))
+    ssc.checkpoint("cp")
+
+    val lines: ReceiverInputDStream[String] = ssc.socketTextStream("localhost", 9999)
+    val wordToOne: DStream[(String, Int)] = lines.map((_, 1))
+
+    val windowDS: DStream[(String, Int)] =
+      wordToOne.reduceByKeyAndWindow(
+        (x: Int, y: Int) => {
+          x + y
+        },
+        (x: Int, y: Int) => {
+          x - y
+        },
+        Seconds(9), Seconds(3))
+
+    // foreachRDD不会出现时间戳
+    windowDS.foreachRDD(
+      rdd => {
+        // 此处根据业务对rdd进行一系列的操作
+      }
+    )
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
